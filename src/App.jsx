b5cfbf76fd92f34358a01531369c9dad3eb5fed6
Mitesh <162997102+miteshpipaliya@users.noreply.github.com>
2026-03-07 22:21:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Leaderboard from "./pages/Leaderboard";
 import Discussion  from "./pages/Discussion";
 import Profile     from "./pages/Profile";
 import Settings    from "./pages/Settings";
+import History     from "./pages/History";
 
 function Inner() {
   const { user, darkMode } = useApp();
@@ -38,6 +39,7 @@ function Inner() {
     "discussion":  <Discussion />,
     "profile":     <Profile setPage={setPage} />,
     "settings":    <Settings />,
+    "history":     <History />,
   };
 
   return (
